d42b5dca19b0ced667da63ad3b35bb6d5903b827
knguyen111601 <knguyen111601@gmail.com>
2022-06-29 20:31:46 -0700

    method for dynamically changing classes dependent on state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 class Counter extends Component {
     state = {
-        count: 0
+        count: 0,
     }
 
     render() {
@@ -10,11 +10,18 @@ class Counter extends Component {
         // React needs a parent component (div here) because React.createElement() takes one param
         return (
             // <>same as these</> 
-            <React.Fragment> 
-                <span>{this.formatCount()}</span>
-                <button>Increment</button>
+            <React.Fragment>
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button className="btn btn-secondary btn-sm">Increment</button>
             </React.Fragment>
         )
+    }
+
+    // Dynamic class method 
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-"
+        classes += (this.state.count === 0) ? "warning" : "primary"
+        return classes
     }
 
     // method
